4a0816ac6d4ffba2d3a35d140c35c704ddc2a5a6
pemakhov <serhiy.pemakhov@gmail.com>
2020-03-06 19:37:34 +0200

    feat: refactor the script using OOP approach
    Organize code into the appropriate classes.

--- src/.tmp/email-grabber.js
+++ src/.tmp/email-grabber.js
@@ -2,22 +2,10 @@ const puppeteer = require('puppeteer');
 const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
-const WEB_PAGE = 'http://localhost:3000/v1/users';
-const MONGODB_URI = 'mongodb://localhost:27017/';
-const MONGODB_DB_MAIN = 'grabbed_data';
-const MONGO_URI = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
-
-const connectOptions = {
-    autoReconnect: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
-
-const connections = mongoose.createConnection(MONGO_URI, connectOptions);
-
-const GrabbedEmailsSchema = new Schema(
+/**
+ * Schema describing database name and fields
+ */
+const grabbedEmailsSchema = new Schema(
     {
         email: {
             type: String,
@@ -30,28 +18,77 @@ const GrabbedEmailsSchema = new Schema(
     }
 );
 
-const connectionsModel = connections.model('GrabbedEmailsModel', GrabbedEmailsSchema);
+/**
+ * Class managing working with database
+ */
+class DbManager {
+    connectOptions = {
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
 
+    constructor(grabbedEmailsSchema) {
+        this.MONGODB_URI = 'mongodb://localhost:27017/';
+        this.MONGODB_DB_MAIN = 'grabbed_data';
+        this.MONGO_URI = `${this.MONGODB_URI}${this.MONGODB_DB_MAIN}`;
 
-const grabEmails = new Promise(async (resolve) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto(WEB_PAGE);
-
-    const body = await page.evaluate(() => document.body.innerHTML);
-    const emailMatcher = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-
-    await browser.close();
-
-    resolve(body.match(emailMatcher));
-});
-
-const saveEmails = (emails) => {
-    try {
-        emails.forEach((email) => connectionsModel.create({ email }));
-    } catch (error) {
+        this.connections = mongoose.createConnection(this.MONGO_URI, this.connectOptions);
+        this.connectionsModel = this.connections.model('GrabbedEmailsModel', grabbedEmailsSchema);
     }
+
+    /**
+     * Saves emails into database collection
+     * @param {*} emails the array of valid emails
+     */
+    async saveEmails(emails) {
+        try {
+            emails.forEach((email) => this.connectionsModel.create({ email }));
+        } catch (error) { }
+    };
 };
 
-grabEmails.then(saveEmails).then(() => console.log('The end of grabbing process.'));
+/**
+ * Class getting web-page html and extracting emails
+ */
+class EmailGrabber {
+    constructor() {
+        this.WEB_PAGE = 'http://localhost:3000/v1/users';
+    }
+
+    /**
+     * Gets web page body
+     * @returns html of the page body
+     */
+    async getPageBody() {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.goto(this.WEB_PAGE);
+
+        const body = await page.evaluate(() => document.body.innerHTML);
+
+        await browser.close();
+        return body;
+    }
+
+    /**
+     * Extracts emails from the html
+     * @returns the array of valid emails
+     */
+    async grabEmails() {
+        const emailMatcher = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+        const body = await this.getPageBody();
+
+        return body.match(emailMatcher);
+    };
+}
+
+const dbManager = new DbManager(grabbedEmailsSchema);
+const emailGrabber = new EmailGrabber();
+
+emailGrabber.grabEmails()
+    .then((emails) => dbManager.saveEmails(emails))
+    .then(() => console.log('The end of grabbing process.'));
