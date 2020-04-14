41a0dedca70458b20a85c4858d0360eed331686e
pemakhov <serhiy.pemakhov@gmail.com>
2020-04-14 12:25:38 +0300

    chore: change eslintrc
    Minor changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,21 @@
 module.exports = {
-    root: true,
     env: {
         browser: true,
+        commonjs: true,
         es6: true,
         node: true,
     },
     extends: [
         'airbnb-base',
-        'plugin:@typescript-eslint/recommended'
     ],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
-    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2018,
-        sourceType: 'module',
     },
-    plugins: [
-        '@typescript-eslint',
-    ],
     rules: {
         indent: ['error', 4],
-        'no-console': 1,
-        'no-param-reassign': "off",
-        '@typescript-eslint/typedef': [
-            'error',
-            {
-                'arrowParameter': true,
-                'variableDeclaration': true,
-                'propertyDeclaration': true
-            }
-        ],
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                'js': 'never',
-                'jsx': 'never',
-                'ts': 'never',
-                'tsx': 'never'
-            }
-        ],
     },
-    settings: {
-        "import/resolver": {
-            "node": {
-                "extensions": [
-                    ".js",
-                    ".ts",
-                ]
-            }
-        }
-    }
 };
