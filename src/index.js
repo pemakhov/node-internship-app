9863fcca412ea1218291a8e723a44ec2ee14382a
pemakhov <serhiy.pemakhov@gmail.com>
2020-04-16 15:08:34 +0300

    feat: process main page request and farther requests separately
    Implement distinguishing between first request and probable additional requests. Remove not used
    libraries. Modify readme.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const http = require('http');
 const url = require('url');
 const httpProxy = require('http-proxy');
-const etag = require('etag');
 const service = require('./service');
 
 /**
@@ -11,30 +10,47 @@ const service = require('./service');
 const PORT = 3000;
 
 /**
+ * Host, parsed from url
+ */
+const mainHost = {
+    name: '',
+    isFresh: false,
+};
+
+/**
  * Proxy server
  */
 const proxy = httpProxy.createProxyServer({});
 
+/**
+ * Http server with proxy
+ */
 const server = http.createServer((req, res) => {
     try {
         const { host } = url.parse(req.url, true).query;
-        console.log(host);
-        console.log(req.url);
+
+        if (host) {
+            mainHost.name = host;
+            mainHost.isFresh = true;
+        } else {
+            // This is needed for inserting the data only for the first request
+            mainHost.isFresh = false;
+        }
+        const currentUrl = host || mainHost.name.concat(req.url);
 
         proxy.web(req, res, {
-            target: host,
+            target: currentUrl,
             changeOrigin: true,
             followRedirects: true,
             selfHandleResponse: true,
         });
     } catch (error) {
         console.error(error);
-        res.end(); // Interrupts loading if the URL is not proper
     }
 });
 
 /**
- * Changes and passes farther data
+ * Changes data if needed and proxies it
  * @param {Event} proxyRes
  * @param {Request} req
  * @param {Response} res
@@ -48,20 +64,22 @@ const onProxyRes = (proxyRes, req, res) => {
         });
 
         proxyRes.on('end', async () => {
-            // Concatinated raw data
-            const data = Buffer.concat(bodyChunks);
-            // Unzipped data
-            const html = await service.unzip(data);
-            // Html code with additional data
-            const markedHtml = service.markWithPhrase(html.toString());
-            // Compressed data
-            const result = await service.gzip(markedHtml);
+            // Concatinate raw data
+            let data = Buffer.concat(bodyChunks);
 
-            res.setHeader('Content-Encoding', 'gzip');
-            res.setHeader('Content-Length', Buffer.byteLength(result, 'utf-8'));
-            res.setHeader('ETag', etag(result));
-            res.setHeader('Cache-Control', 'max-age=3600');
-            res.write(result);
+            if (mainHost.isFresh) {
+                // Unzipped data
+                const html = await service.unzip(data);
+                // Html code with additional data
+                const markedHtml = service.markWithPhrase(html.toString());
+                // Compress data
+                data = await service.gzip(markedHtml);
+
+                res.setHeader('Content-Encoding', 'gzip');
+                res.setHeader('Content-Length', Buffer.byteLength(data, 'utf-8'));
+            }
+
+            res.write(data);
             res.end();
         });
     } catch (error) {
--- src/service.js
+++ src/service.js
@@ -32,7 +32,7 @@ const gzip = (buffer) => new Promise((resolve, reject) => {
 const phrase = 'Hello world!';
 
 /**
- * Html-code containing a phrase to insert before </body> tag
+ * Html-code containing a phrase to insert instead of </body> tag
  */
 const replacement = `<h1 style="position: absolute; top: 10px; left: 20px;
                     color: aqua; z-index: 1000;">${phrase}</h1></body>`;
